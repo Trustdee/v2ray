b59e09b3cc9a8ba5dfe28a4e78c7c62c0ba0306d
Trustdee <Trustokereke@gmail.com>
2019-04-02 17:59:28 +0100

    Update reconfig.sh

--- tools/reconfig.sh
+++ tools/reconfig.sh
@@ -1,75 +1,75 @@
 #!/bin/bash
-uuid=$(cat /proc/sys/kernel/random/uuid)
-cat >/etc/v2ray/233blog_v2ray_backup.conf.tmp <<-EOF
+Uuid=$(cat /proc/sys/kernel/random/uuid)
+Cat >/etc/v2ray/233blog_v2ray_backup.conf.tmp <<-EOF
 # -----------------------------------
-# 警告...请不要修改或删除这个文件...谢谢
-# 警告...请不要修改或删除这个文件...谢谢
-# 警告...请不要修改或删除这个文件...谢谢
+# 警告...Please do not modify or delete this file...thanks
+# 警告...Please do not modify or delete this file...thanks
+# 警告...Please do not modify or delete this file...thanks
 # -----------------------------------
 
-# ---- 再次提醒 ----
-# 大胸弟...如果你看到了这个...记得不要修改或更改这个文件
+# ---- Remind again----
+#大胸弟... If you see this... remember not to modify or change this file
 
-# ---- 说明 ----
-# 嗯……这个文件呢，是用来备份一些设置的
-# 我懒...不想用 JQ 去解析 JSON....
-# 那就把 V2Ray 配置文件的一些重要参数提取出来
-# 然后..在修改 V2Ray 配置的时候再重写一下就 OK 啦...
-# 嗯…笨笨的方法
+# ---- Description----
+#嗯...This file is used to back up some settings.
+#我懒... I don't want to use JQ to parse JSON....
+# Then extract some important parameters of the V2Ray configuration file
+# Then.. When you modify the V2Ray configuration, rewrite it and it will be OK...
+#嗯...Stupid way
 
-# ---- V2Ray 传输协议 -----
-v2ray_transport=$v2ray_transport
+# ---- V2Ray Transmission Protocol-----
+V2ray_transport=$v2ray_transport
 
-#---- V2Ray 端口 -----
-v2ray_port=$v2ray_port
+#---- V2Ray Port-----
+V2ray_port=$v2ray_port
 
 #---- UUID -----
-v2ray_id=$uuid
+V2ray_id=$uuid
 
 #---- alterId -----
 alterId=233
 
-#---- V2Ray 动态端口开始 -----
+#---- V2Ray Dynamic Port Start-----
 v2ray_dynamicPort_start=$v2ray_dynamicPort_start
 
-#---- V2Ray 动态端口结束 -----
+#---- V2Ray Dynamic Port End-----
 v2ray_dynamicPort_end=$v2ray_dynamicPort_end
 
-#---- 域名 -----
-domain=$domain
+#---- Domain Name-----
+Domain=$domain
 
 #---- caddy -----
-caddy_status=$caddy_installed
+Caddy_status=$caddy_installed
 
 #---- Shadowsocks -----
-shadowsocks_status=$shadowsocks
+Shadowsocks_status=$shadowsocks
 
-#---- Shadowsocks 端口 -----
-ssport=$ssport
+#---- Shadowsocks Port-----
+Ssport=$ssport
 
-#---- Shadowsocks 密码 -----
-sspass=$sspass
+#---- Shadowsocks Password-----
+Sspass=$sspass
 
-#---- Shadowsocks 加密协议 -----
-ssciphers=$ssciphers
+#---- Shadowsocks Encryption Protocol-----
+Ssciphers=$ssciphers
 
-#---- 屏蔽广告 -----
-blocked_ad_status=$is_blocked_ad
+#---- Block ads-----
+Blocked_ad_status=$is_blocked_ad
 
-#---- 网站伪装 -----
-ws_path_status=$is_ws_path
+#---- Website Camouflage-----
+Ws_path_status=$is_ws_path
 
-#---- 伪装的路径 -----
-ws_path=$ws_path
+#---- Camouflaged path -----
+Ws_path=$ws_path
 
-#---- 伪装的网址 -----
-proxy_site=$proxy_site
+#---- Disguised URL -----
+Proxy_site=$proxy_site
 		EOF
-rm -rf $backup
-mv -f /etc/v2ray/233blog_v2ray_backup.conf.tmp /etc/v2ray/233blog_v2ray_backup.conf
-echo
-echo -e " .... 哇哦.. .."
-echo
-echo -e " 请使用命令$yellow v2ray reload $none重新加载配置...以避免发生莫名其妙的问题"
-echo
-exit 1
+Rm -rf $backup
+Mv -f /etc/v2ray/233blog_v2ray_backup.conf.tmp /etc/v2ray/233blog_v2ray_backup.conf
+Echo
+Echo -e " .... wow.. .."
+Echo
+Echo -e " Please reload the configuration with the command $yellow v2ray reload $none...to avoid the inexplicable problem"
+Echo
+Exit 1
